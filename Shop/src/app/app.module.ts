fb90b8f7ee3633cc558a7b8a1ef9bbc5f4a4e8e8
Artem <ignatev.artem.nik@gmail.com>
2021-06-09 17:24:27 +0400

    Форма доставки

--- Shop/src/app/app.module.ts
+++ Shop/src/app/app.module.ts
@@ -1,48 +1,49 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
-import {FormsModule} from "@angular/forms";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './shared/main-layout/main-layout.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {CartPageComponent} from './cart-page/cart-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {QuillModule} from "ngx-quill";
 import {AuthInterseptor} from "./shared/auth.interseptor";
-import { ProductComponent } from './product/product.component';
-import { SortingPipe } from './shared/sorting.pipe';
+import {ProductComponent} from './product/product.component';
+import {SortingPipe} from './shared/sorting.pipe';
 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        MainLayoutComponent,
-        MainPageComponent,
-        ProductPageComponent,
-        CartPageComponent,
-        ProductComponent,
-        SortingPipe,
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    ProductComponent,
+    SortingPipe,
 
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HttpClientModule,
-        QuillModule.forRoot()
-    ],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            multi: true,
-            useClass: AuthInterseptor
-        }
-    ],
-    exports: [
-
-    ],
-    bootstrap: [AppComponent]
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    QuillModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: AuthInterseptor
+    }
+  ],
+  exports: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- Shop/src/app/cart-page/cart-page.component.ts
+++ Shop/src/app/cart-page/cart-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../shared/product.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {OrderService} from "../shared/order.service";
 
 @Component({
   selector: 'app-cart-page',
@@ -10,16 +12,59 @@ export class CartPageComponent implements OnInit {
 
   cartProducts = []
   totalPrice = 0
+  form: FormGroup
+  submitted = false
+
 
   constructor(
-    private productServ: ProductService
-  ) { }
+    private productServ: ProductService,
+    private orderServ: OrderService
+  ) {
+  }
 
   ngOnInit(): void {
-    this.cartProducts =  this.productServ.cartProducts
-    for (let i = 0; i< this.cartProducts.length; i++){
+    this.cartProducts = this.productServ.cartProducts
+    for (let i = 0; i < this.cartProducts.length; i++) {
       this.totalPrice += +this.cartProducts[i].price
     }
+
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      phone: new FormControl(null, Validators.required),
+      address: new FormControl(null, Validators.required),
+      payment: new FormControl('Cash'),
+
+    })
+
   }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+    this.submitted = true
+    const order = {
+      name: this.form.value.name,
+      phone: this.form.value.phone,
+      address: this.form.value.address,
+      payment: this.form.value.payment,
+      orders: this.cartProducts,
+      price: this.totalPrice,
+      date: new Date()
+    }
+
+    console.log(this.form)
+    this.orderServ.create(order).subscribe((res=>{
+      this.form.reset()
+      this.submitted=false
+
+    }))
+  }
+
+  delete(product){
+    this.totalPrice -= +product.price
+    this.cartProducts.splice(this.cartProducts.indexOf(product), 1)
+  }
+
 
 }
